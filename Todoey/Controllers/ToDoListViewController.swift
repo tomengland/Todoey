db760408d47d4dc47bc95d14aa8e50cd45d88351
thomasengland <thomasengland@thomass-imac-pro.local>
2018-07-24 13:48:05 -1000

    Convert to MVC design pattern.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -9,18 +9,18 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
-    var itemArray = [String]()
+    
+    var toDoItems = [ToDoItem]()
     
     let defaults = UserDefaults.standard
-
+    let keyForToDoItems = "ToDoItemsArray"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
+        if let items = defaults.array(forKey: keyForToDoItems) as? [ToDoItem] {
+            toDoItems = items
             
         }
     }
@@ -31,14 +31,16 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = toDoItems[indexPath.row].title
+        cell.accessoryType = toDoItems[indexPath.row].isDone ? .checkmark : .none
+        
         
         return cell
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return toDoItems.count
         
     }
     
@@ -49,14 +51,9 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // did Select Row
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        toDoItems[indexPath.row].isDone = !toDoItems[indexPath.row].isDone
         
-        
-        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -66,15 +63,20 @@ class ToDoListViewController: UITableViewController {
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
+        let newItem = ToDoItem()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once user clicks Add Item on UI Alert.
-            self.itemArray.append(textField.text!)
+
+            newItem.title = textField.text!
+            newItem.isDone = false
+            self.toDoItems.append(newItem)
+            
+            
             self.tableView.reloadData()
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.defaults.set(self.toDoItems, forKey: self.keyForToDoItems)
         }
         alert.addTextField {
             (alertTextField) in
---
+++ Todoey/Model/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  Todoey
+//
+//  Created by THOMAS ENGLAND on 7/24/18.
+//  Copyright © 2018 THOMAS ENGLAND. All rights reserved.
+//
+
+import Foundation
+class ToDoItem {
+    
+    var title : String = ""
+    var isDone : Bool = false
+    
+    
+}
